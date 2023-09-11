75293d9869e2f1227ce3b39698a238c2567e2b22
Damariz Licea <A01369045@itesm.mx>
2023-09-10 23:24:37 -0600

    Documentacion

--- act16/main.cpp
+++ act16/main.cpp
@@ -9,7 +9,18 @@
 
 using namespace std;
 
-bool yesGo(int **arr, int x, int y, int nR, int nC)
+/**
+ * @brief keepGoing
+ * @param arr is the array of the maze
+ * @param x helping to move in the rows
+ * @param y helping to move in the columns
+ * @param nR is number of rows
+ * @param nC is number of columns
+ * @return true if the position is valid, false if not
+ * complexity O(1)
+ */
+
+bool keepGoing(int **arr, int x, int y, int nR, int nC)
 {
     if (x < nR && y < nC && arr[x][y] == 1)
     {
@@ -17,6 +28,18 @@ bool yesGo(int **arr, int x, int y, int nR, int nC)
     }
     return false;
 }
+
+/**
+ * @brief recorringMaze
+ * @param arr is the array of the maze
+ * @param x helping to move in the rows
+ * @param y helping to move in the columns
+ * @param nR is number of rows
+ * @param nC is number of columns
+ * @param solutionArr is the array that will be filled with the solution
+ * @return true if there is a solution, false if not
+ * complexity O(2^n)
+ */
 
 bool recorringMaze(int **arr, int x, int y, int nR, int nC, int **solutionArr)
 {
@@ -26,7 +49,7 @@ bool recorringMaze(int **arr, int x, int y, int nR, int nC, int **solutionArr)
         return true;
     }
 
-    if (yesGo(arr, x, y, nR, nC))
+    if (keepGoing(arr, x, y, nR, nC))
     {
         solutionArr[x][y] = 1;
         if (x == nR - 1 && y == nC - 1)
@@ -46,6 +69,14 @@ bool recorringMaze(int **arr, int x, int y, int nR, int nC, int **solutionArr)
     }
     return false;
 }
+
+/**
+ * @brief main
+ * @param nR you enter the number of rows
+ * @param nC you enter the number of columns
+ * @return int
+ * complexity O(1)
+ */
 
 int main()
 {
