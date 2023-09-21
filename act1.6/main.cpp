923a8f5e21b56d35d6e1b05bbebfe751608aecc7
Ricardo Fernandez <ricardo.a.fernandez.a@gmail.com>
2023-09-21 14:54:08 -0600

    Fixed file structure

--- act1.6/main.cpp
+++ act1.6/main.cpp
@@ -1,46 +1,138 @@
 #include <iostream>
 #include <vector>
+#include <math.h>
 
-using std::cin;
-using std::cout;
-using std::vector;
+// Laberinto Backtracking
+// Ricardo Adolfo Fernández Alvarado A01704813
+// Damariz Licea Carrisoza A01369045
 
+using namespace std;
+
+/**
+ * @brief keepGoing is a function that checks if the position is valid or not to move
+ * @param arr is the array of the maze
+ * @param x helping to move in the rows
+ * @param y helping to move in the columns
+ * @param nR is number of rows
+ * @param nC is number of columns
+ * @return true if the position is valid, false if not
+ * Time complexity O(1)
+ */
+bool keepGoing(int **arr, int x, int y, int nR, int nC)
+{
+    if (x < nR && y < nC && arr[x][y] == 1)
+    {
+        return true;
+    }
+    return false;
+}
+
+/**
+ * @brief recorringMaze is a function that checks if there is a solution or not, and if there is a solution it fills the array with the solution
+ * @param arr is the array of the maze
+ * @param x helping to move in the rows
+ * @param y helping to move in the columns
+ * @param nR is number of rows
+ * @param nC is number of columns
+ * @param solutionArr is the array that will be filled with the solution
+ * @return true if there is a solution, false if not
+ * Time complexity O(2^n)
+ * It follows the order first right then down
+ */
+bool solveMaze(int **arr, int x, int y, int nR, int nC, int **solutionArr)
+{
+    if (x == nR - 1 && y == nC - 1)
+    {
+        solutionArr[x][y] = 1;
+        return true;
+    }
+
+    if (keepGoing(arr, x, y, nR, nC))
+    {
+        solutionArr[x][y] = 1;
+        if (x == nR - 1 && y == nC - 1)
+        {
+            return true;
+        }
+        if (solveMaze(arr, x + 1, y, nR, nC, solutionArr))
+        {
+            return true;
+        }
+        if (solveMaze(arr, x, y + 1, nR, nC, solutionArr))
+        {
+            return true;
+        }
+        solutionArr[x][y] = 0;
+        return false;
+    }
+    return false;
+}
+
+/**
+ * @brief main is the function that calls the other functions
+ * @param nR you enter the number of rows
+ * @param nC you enter the number of columns
+ * @return int
+ * Time complexity O(1)
+ */
 int main()
 {
-  int n, m;
-
-  cout << "Ingrese las dimensiones de la matriz: ";
-  cin >> n >> m;
-
-  if (n < 1 || m < 1)
-  {
-    cout << "Error: las dimensiones de la matriz son inválidas.\n";
-    return -1;
-  }
-
-  vector<vector<int>> labyrinth(n, vector<int>(m, 0));
-
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
+    int nR, nC;
+    cout << "Ingresa el numero de filas y columnas: ";
+    cin >> nR >> nC;
+    if (nR <= 0 || nC <= 0)
     {
-      int buffer;
-      cin >> buffer;
-      if (buffer != 0 && buffer != 1)
-      {
-        cout << "Error: el valor ingresado no es válido.\n";
-        return -1;
-      }
-      labyrinth[i][j] = buffer;
+        cout << "El numero de columnas o filas no es valido" << endl;
+        return 0;
     }
-  }
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
+    int **arr = new int *[nR];
+    for (int i = 0; i < nR; i++)
     {
-      cout << labyrinth[i][j] << " ";
+        arr[i] = new int[nC];
     }
-    cout << "\n";
-  }
+
+    cout << "Ingresa los valores del laberinto: " << endl;
+    for (int i = 0; i < nR; i++)
+    {
+        for (int j = 0; j < nC; j++)
+        {
+            int buffer;
+            cin >> buffer;
+            if (buffer != 0 && buffer != 1)
+            {
+                cout << "El valor no es valido" << endl;
+                return 1;
+            }
+            arr[i][j] = buffer;
+        }
+    }
+
+    int **solutionArr = new int *[nR];
+    for (int i = 0; i < nR; i++)
+    {
+        solutionArr[i] = new int[nC];
+        for (int j = 0; j < nC; j++)
+        {
+            solutionArr[i][j] = 0;
+        }
+    }
+    if (solveMaze(arr, 0, 0, nR, nC, solutionArr))
+    {
+        cout << "Si hay solucion" << endl;
+        for (int i = 0; i < nR; i++)
+        {
+            for (int j = 0; j < nC; j++)
+            {
+                cout << solutionArr[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+    else
+    {
+        cout << "No hay solucion" << endl;
+    }
+
+    return 0;
 }
